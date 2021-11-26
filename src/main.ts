443d7f3ab9e0279ed5ed0b3f2694a4b2fd9fb193
MaximeChone <31918084+MaximeChone@users.noreply.github.com>
2021-11-26 19:25:04 +0100

    Swagger is here

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Quizzine')
+    .setDescription("API de l' application Quizzine")
+    .setVersion('1.0')
+    .addTag('users')
+    .addTag('questions')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
