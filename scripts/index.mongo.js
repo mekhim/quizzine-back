35d4c91bba92a98d3d2ce4251a205ca4ae7f15dc
MaximeChone <31918084+MaximeChone@users.noreply.github.com>
2021-12-02 12:29:34 +0100

    no index file

--- scripts/index.mongo.js
+++
@@ -1,7 +0,0 @@
-/**
- * This script is to create index inside the collection users of the database quizzine
- * You can use it with mongo-shell or a tool like Robo3T
- */
-db.getCollection('users').createIndex({ username: 1 }, { unique: true });
-db.getCollection('users').createIndex({ email: 1 }, { unique: true });
-db.getCollection('tags').createIndex({ name: 1 }, { unique: true });
